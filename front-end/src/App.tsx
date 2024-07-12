eb05fe9ce59c6b1203f4cbf2459edfca13fb2e14
mung96 <mung5079@gmail.com>
2024-07-12 10:53:11 +0900

    Setting: zustand 설정 + 테스트

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,12 @@
 import { TestIcon } from "./assets/svg";
+import Counter from "./zustandTest/ZustandTest";
 
 function App() {
   return (
     <div>
       <TestIcon width={"4rem"} />
       Hello! Qufit
+      <Counter />
     </div>
   );
 }
