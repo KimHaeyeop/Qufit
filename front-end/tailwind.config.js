16c296e3e933e06332862cadea7a4cc1a3c19e93
Your Name <you@example.com>
2024-07-16 17:23:16 +0900

    Setting: tailwind custom style 설정

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            backgroundImage: {
+                whitePink:
+                    'linear-gradient(to bottom left,rgba(255, 231, 239, 1) 0%, rgba(255, 255, 255, 0.2) 50%, rgba(255, 255, 255, 0) 100%)',
+            },
+        },
+    },
+    plugins: [],
 };
