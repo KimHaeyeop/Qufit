9c3e865408bd0c869db624fbefb3b1a5548a62ca
mung96 <mung5079@gmail.com>
2024-07-16 17:07:43 +0900

    #S11P12A209-27 #in-progress #comment FE Feat: 라우터 구현

---
+++ front-end/src/routers/Router.tsx
@@ -0,0 +1,66 @@
+import Layout from '@components/Layout';
+import ChattingPage from '@pages/ChattingPage';
+import GroupVideoPage from '@pages/GroupVideoPage';
+import IntroductionPage from '@pages/IntroductionPage';
+import MainPage from '@pages/MainPage';
+import MyPage from '@pages/Mypage';
+import NotFoundPage from '@pages/NotFoundPage';
+import PersonalVideoPage from '@pages/PersonalVideoPage';
+import SignupPage from '@pages/SignupPage';
+import { PATH } from '@routers/PathConstants';
+import { RouterProvider, createBrowserRouter, RouteObject } from 'react-router-dom';
+
+const Router = () => {
+    const routes: RouteObject[] = [
+        //header가 있는 페이지
+        {
+            path: PATH.ROOT,
+            element: <Layout hasHeader={true} />,
+            errorElement: <NotFoundPage />,
+            children: [
+                {
+                    path: PATH.SIGN_UP,
+                    element: <SignupPage />,
+                },
+                {
+                    path: PATH.MAIN,
+                    element: <MainPage />,
+                },
+                {
+                    path: PATH.INTRODUCTION,
+                    element: <IntroductionPage />,
+                },
+                {
+                    path: PATH.CHATTING,
+                    element: <ChattingPage />,
+                },
+                {
+                    path: PATH.MY_PAGE,
+                    element: <MyPage />,
+                },
+            ],
+        },
+
+        //header가 없는 부분
+        {
+            path: PATH.ROOT,
+            element: <Layout hasHeader={false} />,
+            errorElement: <NotFoundPage />,
+            children: [
+                {
+                    path: PATH.GROUP_VIDEO(':roomId'),
+                    element: <GroupVideoPage />,
+                },
+                {
+                    path: PATH.PERSONAL_VIDEO(':roomId'),
+                    element: <PersonalVideoPage />,
+                },
+            ],
+        },
+    ];
+
+    const router = createBrowserRouter([...routes]);
+    return <RouterProvider router={router} />;
+};
+
+export default Router;
