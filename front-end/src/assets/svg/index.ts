2403714805c36d2704f6d909eab197a01a440fc6
박세은 <seeunbag30@gmail.com>
2024-07-17 17:42:40 +0900

    Asset: Layout 구성에 필요한 png, svg파일 추가

---
+++ front-end/src/assets/svg/index.ts
@@ -0,0 +1,4 @@
+export { default as ChatButtonIcon } from './ChatButtonIcon';
+export { default as Logo } from './Logo';
+export { default as MyPageIcon } from './MyPageIcon';
+export { default as NotiIcon } from './NotiIcon';
