152d7b5bb49205b0deb3a29442cfd395bfa18e83
mung96 <mung5079@gmail.com>
2024-07-22 17:05:19 +0900

    Feat: 방장 표시 기능 구현

--- front-end/src/components/video/VideoComponent.tsx
+++ front-end/src/components/video/VideoComponent.tsx
@@ -1,15 +1,19 @@
 import { CrownIcon, MicOffIcon, MicOnIcon } from '@assets/svg/video';
-import { LocalVideoTrack, RemoteVideoTrack } from 'livekit-client';
+import { LocalVideoTrack, Participant, RemoteVideoTrack } from 'livekit-client';
 import { useEffect, useRef } from 'react';
 
 interface VideoComponentProps {
     track: LocalVideoTrack | RemoteVideoTrack;
     participantIdentity: string;
+    participants?: Participant[];
     local?: boolean;
+    isManager: boolean;
 }
 
-function VideoComponent({ track, participantIdentity, local = false }: VideoComponentProps) {
+function VideoComponent({ track, isManager, participantIdentity, local = false }: VideoComponentProps) {
     const videoElement = useRef<HTMLVideoElement | null>(null);
+    console.log(track);
+    console.log(isManager);
 
     useEffect(() => {
         if (videoElement.current) {
@@ -24,7 +28,7 @@ function VideoComponent({ track, participantIdentity, local = false }: VideoComp
         <div>
             <div></div>
             <div className="relative rounded-xl">
-                <CrownIcon width={'50px'} className="absolute left-2 top-2" />
+                {isManager && <CrownIcon width={'50px'} className="absolute left-2 top-2" />}
 
                 <video ref={videoElement} className="h-64 p-0 rounded-xl" />
                 <MicOffIcon width={'30px'} className="absolute  bottom-4 left-full -translate-x-[calc(100%+1rem)]" />
