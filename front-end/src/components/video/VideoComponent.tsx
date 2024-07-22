edc2bc8540292926524b7e375def1dc42db90108
mung96 <mung5079@gmail.com>
2024-07-22 11:17:57 +0900

    Design: 화면 컴포넌트 크기 조정

--- front-end/src/components/video/VideoComponent.tsx
+++ front-end/src/components/video/VideoComponent.tsx
@@ -1,3 +1,4 @@
+import { CrownIcon, MicOffIcon, MicOnIcon } from '@assets/svg/video';
 import { LocalVideoTrack, RemoteVideoTrack } from 'livekit-client';
 import { useEffect, useRef } from 'react';
 
@@ -14,7 +15,6 @@ function VideoComponent({ track, participantIdentity, local = false }: VideoComp
         if (videoElement.current) {
             track.attach(videoElement.current);
         }
-
         return () => {
             track.detach();
         };
@@ -22,10 +22,17 @@ function VideoComponent({ track, participantIdentity, local = false }: VideoComp
 
     return (
         <div>
-            <div>
-                <p>{participantIdentity + (local ? ' (You)' : '')}</p>
+            <div></div>
+            <div className="relative rounded-xl">
+                <CrownIcon width={'50px'} className="absolute left-2 top-2" />
+
+                <video ref={videoElement} className="h-64 p-0 rounded-xl" />
+                <MicOffIcon width={'30px'} className="absolute  bottom-4 left-full -translate-x-[calc(100%+1rem)]" />
+                <MicOnIcon width={'30px'} className="absolute  bottom-4 left-full -translate-x-[calc(100%+1rem)]" />
+                <p className="absolute bottom-4 left-4 text-white font-bold text-sm">
+                    {participantIdentity + (local ? ' (You)' : '')}
+                </p>
             </div>
-            <video ref={videoElement} id={track.sid} className="w-[300px]"></video>
         </div>
     );
 }
