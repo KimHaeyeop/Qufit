9475668b0adf7712140ef8044ea3cd1a31f0f7be
mung96 <mung5079@gmail.com>
2024-07-22 17:05:19 +0900

    Feat: 방장 표시 기능 구현

--- front-end/src/components/video/LiveKitTest.tsx
+++ front-end/src/components/video/LiveKitTest.tsx
@@ -1,10 +1,17 @@
 import {
+    AudioTrack,
     LocalVideoTrack,
+    Participant,
+    ParticipantEvent,
     RemoteParticipant,
     RemoteTrack,
     RemoteTrackPublication,
+    RemoteVideoTrack,
     Room,
     RoomEvent,
+    Track,
+    TrackPublication,
+    VideoTrack,
 } from 'livekit-client';
 import { useState } from 'react';
 import VideoComponent from './VideoComponent';
@@ -16,9 +23,26 @@ type TrackInfo = {
     participantIdentity: string;
 };
 
+interface IParticipant {
+    participant: Participant;
+    createdDate: Date;
+}
+interface IParticipantVideoInfo {
+    [key: string]: { videoTrack: Track; trackPublication: TrackPublication };
+}
+interface IParticipantAudioInfo {
+    [key: string]: { audioTrack: Track; trackPublication: TrackPublication };
+}
 const LIVEKIT_URL = import.meta.env.VITE_LIVEKIT_URL;
 
-function App() {
+function LiveKitTest() {
+    //Participant를 배열에 저장
+    //track, publication을 객체에 저장
+    const [participants, setParticipants] = useState<IParticipant[]>([]);
+    const [localParaticipant, setLocalParticipant] = useState<Participant>();
+    const [participantsVideoInfo, setParticipantsVideoInfo] = useState<IParticipantVideoInfo>({});
+    const [participantsAudioInfo, setParticipantsAudioInfo] = useState<IParticipantAudioInfo>({});
+
     const [room, setRoom] = useState<Room | undefined>(undefined);
     const [localTrack, setLocalTrack] = useState<LocalVideoTrack | undefined>(undefined);
     const [remoteTracks, setRemoteTracks] = useState<TrackInfo[]>([]);
@@ -30,27 +54,42 @@ function App() {
     const [participantName, setParticipantName] = useState('Participant' + Math.floor(Math.random() * 100));
     const [roomName, setRoomName] = useState('Test Room');
 
+    const getManagerSid = () => {
+        const arr = participants.sort((a, b) => {
+            if (a.createdDate > b.createdDate) return 1;
+            else return -1;
+        });
+        return arr[0].participant.sid;
+    };
     const handleTrackUnsubscribed = (track: RemoteTrack, publication: RemoteTrackPublication) => {
         setRemoteTracks((prev) =>
             prev.filter((track) => {
-                console.log(track);
                 return track.trackPublication.trackSid !== publication.trackSid;
             }),
         );
         console.log(remoteTracks);
     };
+
     const handleTrackSubscribed = (
         track: RemoteTrack,
         publication: RemoteTrackPublication,
         participant: RemoteParticipant,
     ) => {
-        setRemoteTracks((prev) => [
-            ...prev,
-            {
-                trackPublication: publication,
-                participantIdentity: participant.identity,
-            },
-        ]);
+        track.kind === 'video' &&
+            setParticipants((prev) => [...prev, { participant: participant, createdDate: new Date() }]);
+
+        track.kind === 'video' &&
+            setParticipantsVideoInfo((prev) => ({
+                ...prev,
+                [participant.sid]: { videoTrack: track, trackPublication: publication },
+            }));
+
+        track.kind === 'audio' &&
+            setParticipantsAudioInfo((prev) => ({
+                ...prev,
+                [participant.sid]: { audioTrack: track, trackPublication: publication },
+            }));
+
         track.kind === 'video'
             ? setVideoRemoteTracks((prev) => [
                   ...prev,
@@ -73,13 +112,13 @@ function App() {
 
         room.on(
             //TrackSubscribed가 오디오할떄도 작동, 비디오할때도 작동이니까 2개가 계속 쌓였던거구나.
+            //내 입장기준 다른 사람의 Track이 구독되는 시점이구나
             RoomEvent.TrackSubscribed,
             (track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => {
                 handleTrackSubscribed(track, publication, participant);
             },
         );
 
-        // On every Track destroyed...
         room.on(RoomEvent.TrackUnsubscribed, (track: RemoteTrack, publication: RemoteTrackPublication) => {
             handleTrackUnsubscribed(track, publication);
         });
@@ -89,6 +128,8 @@ function App() {
             await room.connect(LIVEKIT_URL, token);
             await room.localParticipant.enableCameraAndMicrophone();
             setLocalTrack(room.localParticipant.videoTrackPublications.values().next().value.videoTrack);
+            setLocalParticipant(room.localParticipant);
+            setParticipants((prev) => [...prev, { participant: room.localParticipant, createdDate: new Date() }]);
         } catch (error) {
             console.log('There was an error connecting to the room:', (error as Error).message);
             await leaveRoom();
@@ -101,9 +142,9 @@ function App() {
         setRoom(undefined);
         setLocalTrack(undefined);
         setRemoteTracks([]);
+        console.log(getManagerSid());
     }
 
-    console.log(remoteTracks);
     return (
         <>
             <form
@@ -112,7 +153,7 @@ function App() {
                     e.preventDefault();
                 }}
             >
-                <label className="flex gap-4">
+                <label className="flex gap-4" onClick={() => console.log(participants)}>
                     닉네임
                     <input
                         type="text"
@@ -140,28 +181,57 @@ function App() {
             <div className="flex flex-col h-full justify-between">
                 <div className="flex gap-4">
                     {localTrack && (
-                        <VideoComponent track={localTrack} participantIdentity={participantName} local={true} />
+                        <VideoComponent
+                            track={localTrack}
+                            isManager={getManagerSid() === localParaticipant?.sid}
+                            participantIdentity={participantName}
+                            local={true}
+                            // participants={participants}
+                        />
                     )}
 
-                    {videoRemoteTracks.map(
+                    {/* {videoRemoteTracks.map(
                         (remoteTrack, idx) =>
                             idx % 2 === 1 && (
                                 <VideoComponent
+                                    // participants={participants}
                                     key={remoteTrack.trackPublication.trackSid}
                                     track={remoteTrack.trackPublication.videoTrack!}
+                                    isManager={getManagerSid() === remoteTrack.trackPublication.videoTrack?.sid}
                                     participantIdentity={remoteTrack.participantIdentity}
+                                />
+                            ),
+                    )} */}
+                    {participants.map(
+                        (participant, idx) =>
+                            participant.participant.sid !== localParaticipant?.sid &&
+                            idx % 2 === 1 && (
+                                <VideoComponent
+                                    // participants={participants}
+                                    key={participant.participant.sid}
+                                    track={
+                                        participantsVideoInfo[participant.participant.sid]
+                                            .videoTrack as RemoteVideoTrack
+                                    }
+                                    isManager={getManagerSid() === participant.participant.sid}
+                                    participantIdentity={participant.participant.name || ''}
                                 />
                             ),
                     )}
                 </div>
                 <div className="flex gap-4">
-                    {videoRemoteTracks.map(
-                        (remoteTrack, idx) =>
+                    {participants.map(
+                        (participant, idx) =>
+                            participant.participant.sid !== localParaticipant?.sid &&
                             idx % 2 === 0 && (
                                 <VideoComponent
-                                    key={remoteTrack.trackPublication.trackSid}
-                                    track={remoteTrack.trackPublication.videoTrack!}
-                                    participantIdentity={remoteTrack.participantIdentity}
+                                    key={participant.participant.sid}
+                                    track={
+                                        participantsVideoInfo[participant.participant.sid]
+                                            .videoTrack as RemoteVideoTrack
+                                    }
+                                    isManager={getManagerSid() === participant.participant.sid}
+                                    participantIdentity={participant.participant.name || ''}
                                 />
                             ),
                     )}
@@ -179,4 +249,4 @@ function App() {
     );
 }
 
-export default App;
+export default LiveKitTest;
