23c0382370f3bf6f2cae6256cf062c4f5a967950
mung96 <mung5079@gmail.com>
2024-07-23 16:58:53 +0900

    Refactor: 비디오 페이지 레이아웃 분리

--- front-end/src/components/common/Layout.tsx
+++ front-end/src/components/common/Layout.tsx
@@ -1,7 +1,7 @@
 import Header from '@components/common/Header';
 import { Outlet, useLocation } from 'react-router-dom';
 
-const Layout = ({ hasHeader }: { hasHeader: boolean }) => {
+const Layout = () => {
     const location = useLocation();
     const isLocation = location.pathname;
 
@@ -9,7 +9,7 @@ const Layout = ({ hasHeader }: { hasHeader: boolean }) => {
 
     return (
         <div className={`flex flex-col items-center justify-center w-screen h-screen bg-cover px-14 ${background}`}>
-            {hasHeader && <Header />}
+            <Header />
             <div className="w-full aspect-layout effect-layout border-b-4 border-x-2 border-lightPurple-4 rounded-b-[2.75rem] relative">
                 <Outlet />
                 <div className="z-0 w-full h-full rounded-b-[2.75rem] bg-whitePink opacity-20" />
