48de74d8eddae34798d969902d201cc0747a004f
박세은 <seeunbag30@gmail.com>
2024-07-23 09:27:20 +0900

    Asset: 채팅창에 쓸 아이콘 추가

--- front-end/src/assets/svg/chat/index.ts
+++ front-end/src/assets/svg/chat/index.ts
@@ -3,3 +3,4 @@ export { default as DeleteFriendIcon } from './DeleteFriendIcon';
 export { default as DoorExitIcon } from './DoorExitIcon';
 export { default as EmptyChatIcon } from './EmptyChatIcon';
 export { default as StartChatIcon } from './StartChatIcon';
+export { default as XIcon } from './XIcon';
