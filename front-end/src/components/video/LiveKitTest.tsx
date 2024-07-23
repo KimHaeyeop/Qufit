1c3679225b66ab9e01e1742784f93893640bf43b
mung96 <mung5079@gmail.com>
2024-07-23 10:56:21 +0900

    Feat: 마이크 on/off 기능 구현

--- front-end/src/components/video/LiveKitTest.tsx
+++ front-end/src/components/video/LiveKitTest.tsx
@@ -2,7 +2,6 @@ import {
     AudioTrack,
     LocalVideoTrack,
     Participant,
-    ParticipantEvent,
     RemoteParticipant,
     RemoteTrack,
     RemoteTrackPublication,
@@ -11,12 +10,12 @@ import {
     RoomEvent,
     Track,
     TrackPublication,
-    VideoTrack,
 } from 'livekit-client';
 import { useState } from 'react';
 import VideoComponent from './VideoComponent';
 import AudioComponent from './AudioComponent';
 import { getToken } from '@components/video/getToken';
+import { useRoomStore } from '@stores/video/roomStore';
 
 type TrackInfo = {
     trackPublication: RemoteTrackPublication;
@@ -36,23 +35,19 @@ interface IParticipantAudioInfo {
 const LIVEKIT_URL = import.meta.env.VITE_LIVEKIT_URL;
 
 function LiveKitTest() {
-    //Participant를 배열에 저장
-    //track, publication을 객체에 저장
     const [participants, setParticipants] = useState<IParticipant[]>([]);
     const [localParaticipant, setLocalParticipant] = useState<Participant>();
     const [participantsVideoInfo, setParticipantsVideoInfo] = useState<IParticipantVideoInfo>({});
     const [participantsAudioInfo, setParticipantsAudioInfo] = useState<IParticipantAudioInfo>({});
 
-    const [room, setRoom] = useState<Room | undefined>(undefined);
+    const room = useRoomStore((state) => state.room);
+    const setRoom = useRoomStore((state) => state.setRoom);
+
     const [localTrack, setLocalTrack] = useState<LocalVideoTrack | undefined>(undefined);
     const [remoteTracks, setRemoteTracks] = useState<TrackInfo[]>([]);
 
-    const [videoRemoteTracks, setVideoRemoteTracks] = useState<TrackInfo[]>([]);
-
-    const [audioRemoteTracks, setAudioRemoteTracks] = useState<TrackInfo[]>([]);
-
-    const [participantName, setParticipantName] = useState('Participant' + Math.floor(Math.random() * 100));
-    const [roomName, setRoomName] = useState('Test Room');
+    const roomName = 'test Room';
+    const participantName = String(Math.random() * 100);
 
     const getManagerSid = () => {
         const arr = participants.sort((a, b) => {
@@ -61,6 +56,7 @@ function LiveKitTest() {
         });
         return arr[0].participant.sid;
     };
+
     const handleTrackUnsubscribed = (track: RemoteTrack, publication: RemoteTrackPublication) => {
         setRemoteTracks((prev) =>
             prev.filter((track) => {
@@ -89,30 +85,21 @@ function LiveKitTest() {
                 ...prev,
                 [participant.sid]: { audioTrack: track, trackPublication: publication },
             }));
-
-        track.kind === 'video'
-            ? setVideoRemoteTracks((prev) => [
-                  ...prev,
-                  {
-                      trackPublication: publication,
-                      participantIdentity: participant.identity,
-                  },
-              ])
-            : setAudioRemoteTracks((prev) => [
-                  ...prev,
-                  {
-                      trackPublication: publication,
-                      participantIdentity: participant.identity,
-                  },
-              ]);
     };
+
     async function joinRoom() {
         const room = new Room();
         setRoom(room);
 
         room.on(
-            //TrackSubscribed가 오디오할떄도 작동, 비디오할때도 작동이니까 2개가 계속 쌓였던거구나.
-            //내 입장기준 다른 사람의 Track이 구독되는 시점이구나
+            RoomEvent.TrackSubscribed,
+            (track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => {
+                publication.setEnabled(true);
+                console.log(publication);
+            },
+        );
+
+        room.on(
             RoomEvent.TrackSubscribed,
             (track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => {
                 handleTrackSubscribed(track, publication, participant);
@@ -137,45 +124,17 @@ function LiveKitTest() {
     }
 
     async function leaveRoom() {
-        console.log(remoteTracks);
         await room?.disconnect();
         setRoom(undefined);
         setLocalTrack(undefined);
         setRemoteTracks([]);
-        console.log(getManagerSid());
     }
-
+    console.log(room);
     return (
         <>
-            <form
-                onSubmit={(e) => {
-                    joinRoom();
-                    e.preventDefault();
-                }}
-            >
-                <label className="flex gap-4" onClick={() => console.log(participants)}>
-                    닉네임
-                    <input
-                        type="text"
-                        placeholder="닉네임 입력"
-                        onChange={(e) => setParticipantName(e.target.value)}
-                        required
-                    />
-                </label>
-                <label className="flex gap-4">
-                    Room
-                    <input
-                        placeholder="방이름 입력"
-                        value={11}
-                        type="text"
-                        onChange={(e) => setRoomName(e.target.value)}
-                        required
-                    />
-                </label>
-                <button type="submit" disabled={!roomName || !participantName}>
-                    입장하기
-                </button>
-            </form>
+            <button onClick={joinRoom} type="submit">
+                입장하기
+            </button>
 
             <button onClick={leaveRoom}>Leave Room</button>
             <div className="flex flex-col h-full justify-between">
@@ -186,22 +145,8 @@ function LiveKitTest() {
                             isManager={getManagerSid() === localParaticipant?.sid}
                             participantIdentity={participantName}
                             local={true}
-                            // participants={participants}
                         />
                     )}
-
-                    {/* {videoRemoteTracks.map(
-                        (remoteTrack, idx) =>
-                            idx % 2 === 1 && (
-                                <VideoComponent
-                                    // participants={participants}
-                                    key={remoteTrack.trackPublication.trackSid}
-                                    track={remoteTrack.trackPublication.videoTrack!}
-                                    isManager={getManagerSid() === remoteTrack.trackPublication.videoTrack?.sid}
-                                    participantIdentity={remoteTrack.participantIdentity}
-                                />
-                            ),
-                    )} */}
                     {participants.map(
                         (participant, idx) =>
                             participant.participant.sid !== localParaticipant?.sid &&
@@ -237,12 +182,18 @@ function LiveKitTest() {
                     )}
                 </div>
                 <div>
-                    {audioRemoteTracks.map((remoteTrack) => (
-                        <AudioComponent
-                            key={remoteTrack.trackPublication.trackSid}
-                            track={remoteTrack.trackPublication.audioTrack!}
-                        />
-                    ))}
+                    {participants.map(
+                        (participant) =>
+                            participant.participant.sid !== localParaticipant?.sid && (
+                                <AudioComponent
+                                    key={participant.participant.audioTrackPublications.size}
+                                    track={
+                                        (participantsAudioInfo[participant.participant.sid].audioTrack as AudioTrack) ||
+                                        ''
+                                    }
+                                />
+                            ),
+                    )}
                 </div>
             </div>
         </>
--- front-end/src/components/video/VideoComponent.tsx
+++ front-end/src/components/video/VideoComponent.tsx
@@ -1,6 +1,7 @@
 import { CrownIcon, MicOffIcon, MicOnIcon } from '@assets/svg/video';
+import { useRoomStore } from '@stores/video/roomStore';
 import { LocalVideoTrack, Participant, RemoteVideoTrack } from 'livekit-client';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 interface VideoComponentProps {
     track: LocalVideoTrack | RemoteVideoTrack;
@@ -12,8 +13,15 @@ interface VideoComponentProps {
 
 function VideoComponent({ track, isManager, participantIdentity, local = false }: VideoComponentProps) {
     const videoElement = useRef<HTMLVideoElement | null>(null);
-    console.log(track);
-    console.log(isManager);
+    const [isMicEnable, setIsMicEnable] = useState(true);
+    // const [isCameraEnable, setIsCameraEnable] = useState(false);
+    const room = useRoomStore((state) => state.room);
+
+    // const test = () => {
+    //     room?.localParticipant.setCameraEnabled(!room?.localParticipant.isCameraEnabled);
+
+    //     console.log(track);
+    // };
 
     useEffect(() => {
         if (videoElement.current) {
@@ -24,15 +32,22 @@ function VideoComponent({ track, isManager, participantIdentity, local = false }
         };
     }, [track]);
 
+    const handleMicIconClick = () => {
+        setIsMicEnable(!room?.localParticipant.isMicrophoneEnabled);
+        room?.localParticipant.setMicrophoneEnabled(!room?.localParticipant.isMicrophoneEnabled);
+    };
     return (
-        <div>
-            <div></div>
+        <div onClick={test}>
             <div className="relative rounded-xl">
                 {isManager && <CrownIcon width={'50px'} className="absolute left-2 top-2" />}
 
                 <video ref={videoElement} className="h-64 p-0 rounded-xl" />
-                <MicOffIcon width={'30px'} className="absolute  bottom-4 left-full -translate-x-[calc(100%+1rem)]" />
-                <MicOnIcon width={'30px'} className="absolute  bottom-4 left-full -translate-x-[calc(100%+1rem)]" />
+                <div
+                    onClick={handleMicIconClick}
+                    className="absolute  bottom-4 left-full -translate-x-[calc(100%+1rem)]"
+                >
+                    {isMicEnable ? <MicOnIcon width={'30px'} /> : <MicOffIcon width={'30px'} />}
+                </div>
                 <p className="absolute bottom-4 left-4 text-white font-bold text-sm">
                     {participantIdentity + (local ? ' (You)' : '')}
                 </p>
