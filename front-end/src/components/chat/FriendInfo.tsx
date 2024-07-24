48c1ec69170d312e5854e7c950fa7fc213ff21b4
박세은 <seeunbag30@gmail.com>
2024-07-24 14:30:34 +0900

    S11P12A209-101 Design: 채팅페이지 반응형 추가

--- front-end/src/components/chat/FriendInfo.tsx
+++ front-end/src/components/chat/FriendInfo.tsx
@@ -26,16 +26,22 @@ const FriendInfo = ({ id, nickname, profileImage }: FriendInfoProps) => {
     return (
         <div className="flex flex-col">
             <div className="w-full h-0.5 bg-hotPink" />
-            <div className="flex items-center justify-between pl-1 pr-9">
-                <div className="flex items-center py-4 ">
-                    <img src={profileImage} alt="user profile image" className="rounded-full w-14 h-14" />
-                    <p className="text-2xl text-white mx-3.5 max-w-72 truncate">{nickname}</p>
+            <div className="flex items-center justify-between pl-1 pr-9 lg:pr-7 xs:pr-2">
+                <div className="flex items-center py-5 lg:py-3">
+                    <img
+                        src={profileImage}
+                        alt="user profile image"
+                        className="rounded-full w-14 h-14 lg:w-12 lg:h-12 xs:w-12 xs:h-12"
+                    />
+                    <p className="text-2xl text-white mx-3.5 max-w-72 truncate lg:text-xl lg:mx-2.5 lg:max-w-60 xs:text-lg xs:max-w-52 xs:mx-2">
+                        {nickname}ddddddddddddddddddddddd
+                    </p>
                     <button>
-                        <DeleteFriendIcon className="w-6" />
+                        <DeleteFriendIcon className="w-6 lg:w-5 md:w-8 xs:w-6 xs:mr-10" />
                     </button>
                 </div>
                 <button onClick={handleStartChatButton}>
-                    <StartChatIcon className="w-8" />
+                    <StartChatIcon className="w-8 lg:w-7 md:w-10" />
                 </button>
             </div>
         </div>
