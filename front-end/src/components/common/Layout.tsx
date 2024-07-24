ac3de16491186f0e44b37e3dce2b7525959fbb9d
박세은 <seeunbag30@gmail.com>
2024-07-24 14:29:35 +0900

    S11P12A209-101 Design: 헤더, 레이아웃 반응형 추가

--- front-end/src/components/common/Layout.tsx
+++ front-end/src/components/common/Layout.tsx
@@ -9,14 +9,14 @@ const Layout = ({ hasHeader }: { hasHeader: boolean }) => {
 
     return (
         <div
-            className={`flex flex-col items-center justify-center w-screen h-screen bg-cover px-14 py-4 ${background} lg:px-20`}
+            className={`flex flex-col items-center justify-center w-screen h-screen bg-cover px-14 py-4 ${background} lg:px-20 md:py-10 sm:py-12 sm:px-6 xs:py-12 xs:px-6`}
         >
             {hasHeader && (
                 <>
                     <Header />
-                    <div className="w-full aspect-layout effect-layout border-b-4 border-x-2 border-lightPurple-4 rounded-b-[2.75rem] relative lg:rounded-b-[2rem]">
+                    <div className="w-full aspect-layout effect-layout border-b-4 border-x-2 border-lightPurple-4 rounded-b-[2.75rem] relative md:h-full lg:rounded-b-[2rem] sm:h-full xs:h-full">
                         <Outlet />
-                        <div className="z-0 w-full h-full rounded-b-[2.75rem] bg-whitePink opacity-20 lg:rounded-b-[2rem]" />
+                        <div className="z-0 w-full h-full rounded-b-[2.75rem] bg-whitePink opacity-20 lg:rounded-b-[2rem] md:h-full sm:h-full xs:h-full" />
                     </div>
                 </>
             )}
