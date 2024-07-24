e1e2d34575ebc4e76d76ed7b2ba195637d6f135b
mung96 <mung5079@gmail.com>
2024-07-24 13:01:48 +0900

    Refactor: 방장 결정하는 로직 수정 (Participants 받아서 활용)

--- front-end/src/pages/GroupVideoPage.tsx
+++ front-end/src/pages/GroupVideoPage.tsx
@@ -1,7 +1,217 @@
-import LiveKitTest from '@components/video/LiveKitTest';
+import {
+    AudioTrack,
+    LocalVideoTrack,
+    Participant,
+    RemoteParticipant,
+    RemoteTrack,
+    RemoteTrackPublication,
+    RemoteVideoTrack,
+    Room,
+    RoomEvent,
+    Track,
+    TrackPublication,
+} from 'livekit-client';
+import { useState } from 'react';
 
-const GroupVideoPage = () => {
-    return <LiveKitTest />;
+import { getToken } from '@components/video/getToken';
+import { useRoomStore } from '@stores/video/roomStore';
+import VideoComponent from '@components/video/VideoComponent';
+import AudioComponent from '@components/video/AudioComponent';
+
+type TrackInfo = {
+    trackPublication: RemoteTrackPublication;
+    participantIdentity: string;
 };
+
+interface IParticipantVideoInfo {
+    [key: string]: { videoTrack: Track; trackPublication: TrackPublication };
+}
+interface IParticipantAudioInfo {
+    [key: string]: { audioTrack: Track; trackPublication: TrackPublication };
+}
+const LIVEKIT_URL = import.meta.env.VITE_LIVEKIT_URL;
+
+function GroupVideoPage() {
+    const [participants, setParticipants] = useState<Participant[]>([]);
+    const [managerName, setManagerName] = useState('');
+
+    const [localParaticipant, setLocalParticipant] = useState<Participant>();
+    const [participantsVideoInfo, setParticipantsVideoInfo] = useState<IParticipantVideoInfo>({});
+    const [participantsAudioInfo, setParticipantsAudioInfo] = useState<IParticipantAudioInfo>({});
+
+    const room = useRoomStore((state) => state.room);
+    const setRoom = useRoomStore((state) => state.setRoom);
+
+    const [localTrack, setLocalTrack] = useState<LocalVideoTrack | undefined>(undefined);
+    const [remoteTracks, setRemoteTracks] = useState<TrackInfo[]>([]);
+
+    const roomName = 'test Room';
+    const participantName = String(Math.random() * 100);
+
+    const handleTrackUnsubscribed = (track: RemoteTrack, publication: RemoteTrackPublication) => {
+        setRemoteTracks((prev) =>
+            prev.filter((track) => {
+                return track.trackPublication.trackSid !== publication.trackSid;
+            }),
+        );
+        console.log(remoteTracks);
+    };
+
+    const handleTrackSubscribed = (
+        track: RemoteTrack,
+        publication: RemoteTrackPublication,
+        participant: RemoteParticipant,
+    ) => {
+        track.kind === 'video' &&
+            setParticipantsVideoInfo((prev) => ({
+                ...prev,
+                [participant.sid]: { videoTrack: track, trackPublication: publication },
+            }));
+
+        track.kind === 'audio' &&
+            setParticipantsAudioInfo((prev) => ({
+                ...prev,
+                [participant.sid]: { audioTrack: track, trackPublication: publication },
+            }));
+    };
+
+    async function joinRoom() {
+        const room = new Room({
+            videoCaptureDefaults: {
+                deviceId: '',
+                facingMode: 'user',
+                resolution: {
+                    width: 355,
+                    height: 260,
+                    frameRate: 30,
+                },
+            },
+        });
+        setRoom(room);
+
+        room.on(
+            RoomEvent.TrackSubscribed,
+            (track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => {
+                publication.setEnabled(true);
+                console.log(publication);
+            },
+        );
+
+        room.on(
+            RoomEvent.TrackSubscribed,
+            (track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => {
+                handleTrackSubscribed(track, publication, participant);
+            },
+        );
+
+        room.on(RoomEvent.TrackUnsubscribed, (track: RemoteTrack, publication: RemoteTrackPublication) => {
+            handleTrackUnsubscribed(track, publication);
+        });
+
+        try {
+            const token = await getToken(roomName, participantName);
+            await room.connect(LIVEKIT_URL, token);
+            await room.localParticipant.enableCameraAndMicrophone();
+
+            //입장시 방장 정하자
+            const curParticipants = [];
+            curParticipants.push(room.localParticipant);
+            Array.from(room.remoteParticipants.values()).forEach((participant) => curParticipants.push(participant));
+            console.log(curParticipants);
+            setParticipants(curParticipants);
+
+            curParticipants.sort((partA, partB) => {
+                if (new Date(partA.joinedAt!).getTime() > new Date(partB.joinedAt!).getTime()) return 1;
+                else return -1;
+            });
+
+            setManagerName(curParticipants[0].name!);
+
+            console.log(room.localParticipant.joinedAt);
+            console.log(Array.from(room.remoteParticipants.values()));
+
+            setLocalTrack(room.localParticipant.videoTrackPublications.values().next().value.videoTrack);
+            setLocalParticipant(room.localParticipant);
+        } catch (error) {
+            console.log('There was an error connecting to the room:', (error as Error).message);
+            await leaveRoom();
+        }
+    }
+
+    async function leaveRoom() {
+        await room?.disconnect();
+        setRoom(undefined);
+        setLocalTrack(undefined);
+        setRemoteTracks([]);
+    }
+    return (
+        <>
+            <button onClick={leaveRoom}>Leave Room</button>
+            {/* <div className="flex flex-col justify-between h-full">
+                <div className="flex gap-4"> */}
+            {/* {localTrack && (
+                        <VideoComponent
+                            track={localTrack}
+                            isManager={localParaticipant?.sid}
+                            participantIdentity={participantName}
+                            local={true}
+                        />
+                    )}
+                    {participants.map(
+                        (participant, idx) =>
+                            participant.participant.sid !== localParaticipant?.sid &&
+                            idx % 2 === 1 && (
+                                <VideoComponent
+                                    // participants={participants}
+                                    key={participant.participant.sid}
+                                    track={
+                                        participantsVideoInfo[participant.participant.sid]
+                                            .videoTrack as RemoteVideoTrack
+                                    }
+                                    isManager={getManagerSid() === participant.participant.sid}
+                                    participantIdentity={participant.participant.name || ''}
+                                />
+                            ),
+                    )}
+                </div> */}
+            <button onClick={joinRoom} type="submit">
+                입장하기
+            </button>
+
+            {/* <div className="flex gap-4">
+                    {participants.map(
+                        (participant, idx) =>
+                            participant.participant.sid !== localParaticipant?.sid &&
+                            idx % 2 === 0 && (
+                                <VideoComponent
+                                    key={participant.participant.sid}
+                                    track={
+                                        participantsVideoInfo[participant.participant.sid]
+                                            .videoTrack as RemoteVideoTrack
+                                    }
+                                    isManager={getManagerSid() === participant.participant.sid}
+                                    participantIdentity={participant.participant.name || ''}
+                                />
+                            ),
+                    )}
+                </div>
+                <div>
+                    {participants.map(
+                        (participant) =>
+                            participant.participant.sid !== localParaticipant?.sid && (
+                                <AudioComponent
+                                    key={participant.participant.audioTrackPublications.size}
+                                    track={
+                                        (participantsAudioInfo[participant.participant.sid].audioTrack as AudioTrack) ||
+                                        ''
+                                    }
+                                />
+                            ),
+                    )}
+                </div>
+            </div> */}
+        </>
+    );
+}
 
 export default GroupVideoPage;
