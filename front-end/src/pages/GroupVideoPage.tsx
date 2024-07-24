719acdefac0483613ac8f4a097a9bd6e462cfe22
mung96 <mung5079@gmail.com>
2024-07-24 13:48:07 +0900

    Refactor: 필요없는 코드 삭제

--- front-end/src/pages/GroupVideoPage.tsx
+++ front-end/src/pages/GroupVideoPage.tsx
@@ -1,16 +1,4 @@
-import {
-    AudioTrack,
-    LocalVideoTrack,
-    Participant,
-    RemoteParticipant,
-    RemoteTrack,
-    RemoteTrackPublication,
-    RemoteVideoTrack,
-    Room,
-    RoomEvent,
-    Track,
-    TrackPublication,
-} from 'livekit-client';
+import { Participant, Room, RoomEvent, Track, TrackPublication } from 'livekit-client';
 import { useState } from 'react';
 
 import { getToken } from '@components/video/getToken';
@@ -18,63 +6,19 @@ import { useRoomStore } from '@stores/video/roomStore';
 import VideoComponent from '@components/video/VideoComponent';
 import AudioComponent from '@components/video/AudioComponent';
 
-type TrackInfo = {
-    trackPublication: RemoteTrackPublication;
-    participantIdentity: string;
-};
-
-interface IParticipantVideoInfo {
-    [key: string]: { videoTrack: Track; trackPublication: TrackPublication };
-}
-interface IParticipantAudioInfo {
-    [key: string]: { audioTrack: Track; trackPublication: TrackPublication };
-}
 const LIVEKIT_URL = import.meta.env.VITE_LIVEKIT_URL;
 
 function GroupVideoPage() {
-    const [participants, setParticipants] = useState<Participant[]>([]);
     const [managerName, setManagerName] = useState('');
-
+    const [participants, setParticipants] = useState<Participant[]>([]);
     const [localParticipant, setLocalParticipant] = useState<Participant>();
     const [remoteParticipants, setRemoteParticipants] = useState<Participant[]>([]);
-    // const [participantsVideoInfo, setParticipantsVideoInfo] = useState<IParticipantVideoInfo>({});
-    // const [participantsAudioInfo, setParticipantsAudioInfo] = useState<IParticipantAudioInfo>({});
 
     const room = useRoomStore((state) => state.room);
     const setRoom = useRoomStore((state) => state.setRoom);
 
-    // const [localTrack, setLocalTrack] = useState<LocalVideoTrack | undefined>(undefined);
-    // const [remoteTracks, setRemoteTracks] = useState<TrackInfo[]>([]);
-
     const roomName = 'test Room';
     const participantName = String(Math.random() * 100);
-
-    // const handleTrackUnsubscribed = (track: RemoteTrack, publication: RemoteTrackPublication) => {
-    //     setRemoteTracks((prev) =>
-    //         prev.filter((track) => {
-    //             return track.trackPublication.trackSid !== publication.trackSid;
-    //         }),
-    //     );
-    //     console.log(remoteTracks);
-    // };
-
-    // const handleTrackSubscribed = (
-    //     track: RemoteTrack,
-    //     publication: RemoteTrackPublication,
-    //     participant: RemoteParticipant,
-    // ) => {
-    //     track.kind === 'video' &&
-    //         setParticipantsVideoInfo((prev) => ({
-    //             ...prev,
-    //             [participant.sid]: { videoTrack: track, trackPublication: publication },
-    //         }));
-
-    //     track.kind === 'audio' &&
-    //         setParticipantsAudioInfo((prev) => ({
-    //             ...prev,
-    //             [participant.sid]: { audioTrack: track, trackPublication: publication },
-    //         }));
-    // };
 
     async function joinRoom() {
         const room = new Room({
@@ -90,28 +34,10 @@ function GroupVideoPage() {
         });
         setRoom(room);
 
-        // room.on(
-        //     RoomEvent.TrackSubscribed,
-        //     (track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => {
-        //         publication.setEnabled(true);
-        //     },
-        // );
-
         room.on(RoomEvent.ParticipantConnected, (participant) => {
             setParticipants((prev) => [...prev, participant]);
             setRemoteParticipants((prev) => [...prev, participant]);
         });
-
-        // room.on(
-        //     RoomEvent.TrackSubscribed,
-        //     (track: RemoteTrack, publication: RemoteTrackPublication, participant: RemoteParticipant) => {
-        //         handleTrackSubscribed(track, publication, participant);
-        //     },
-        // );
-
-        // room.on(RoomEvent.TrackUnsubscribed, (track: RemoteTrack, publication: RemoteTrackPublication) => {
-        //     handleTrackUnsubscribed(track, publication);
-        // });
 
         try {
             const token = await getToken(roomName, participantName);
@@ -122,19 +48,15 @@ function GroupVideoPage() {
             const curParticipants = [];
             curParticipants.push(room.localParticipant);
             Array.from(room.remoteParticipants.values()).forEach((participant) => curParticipants.push(participant));
-            setParticipants(curParticipants);
-
             curParticipants.sort((partA, partB) => {
                 if (new Date(partA.joinedAt!).getTime() > new Date(partB.joinedAt!).getTime()) return 1;
                 else return -1;
             });
 
             setManagerName(curParticipants[0].name!);
+            setParticipants(curParticipants);
             setLocalParticipant(room.localParticipant);
             setRemoteParticipants(Array.from(room.remoteParticipants.values()));
-
-            // setLocalTrack(room.localParticipant.videoTrackPublications.values().next().value.videoTrack);
-            // setLocalParticipant(room.localParticipant);
         } catch (error) {
             console.log('There was an error connecting to the room:', (error as Error).message);
             await leaveRoom();
@@ -144,8 +66,6 @@ function GroupVideoPage() {
     async function leaveRoom() {
         await room?.disconnect();
         setRoom(undefined);
-        // setLocalTrack(undefined);
-        // setRemoteTracks([]);
     }
     return (
         <>
