c23c98247416c3b59e259cea80be9849acab0f08
mung96 <mung5079@gmail.com>
2024-07-29 11:15:50 +0900

    #S11P12A209-121 #comment FE/ Feat : login 이후 응답에 따라 로직 처리

--- front-end/src/pages/KaKaoRedirectPage.tsx
+++ front-end/src/pages/KaKaoRedirectPage.tsx
@@ -1,12 +1,33 @@
+import { HttpStatus } from '@apis/ApiConstants';
 import { getKakaoAccessToken, login } from '@apis/auth/AuthApi';
-import { useSearchParams } from 'react-router-dom';
+import { PATH } from '@routers/PathConstants';
+import { useSetAccessTokenStore } from '@stores/auth/signUpStore';
+import { isAxiosError } from 'axios';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const KakaoRedirectPage = () => {
     const [searchParams] = useSearchParams();
-
+    const navigate = useNavigate();
     const authCode = searchParams.get('code');
+    const setAccessToken = useSetAccessTokenStore();
+    getKakaoAccessToken(authCode!).then((response) => {
+        setAccessToken(response.data.access_token);
+        login(response.data.access_token)
+            .then((response) => console.log(response))
+            .catch((error) => {
+                if (isAxiosError(error)) {
+                    console.log(error);
+                    if (error.response?.data.status === HttpStatus.UNAUTHORIZED) {
+                        if (error.response.data.errorMessage === '회원 가입이 필요합니다.') {
+                            navigate(PATH.SIGN_UP);
+                        } else if (error.response.data.errorMessage === '가입 승인 대기 중인 계정입니다.') {
+                            navigate(PATH.INTRODUCTION);
+                        }
+                    }
+                }
+            });
+    });
 
-    getKakaoAccessToken(authCode!).then((response) => login(response.data.access_token));
     return <div>카카오 리다이렉트 페이지</div>;
 };
 
