5710c7c6c695a92cf1c3f9edfdeaf4be2e78a4aa
mung96 <mung5079@gmail.com>
2024-07-29 10:24:47 +0900

    #S11P12A209-121 #comment FE/ Feat : login 기능 구현

--- front-end/src/apis/ApiConstants.tsx
+++ front-end/src/apis/ApiConstants.tsx
@@ -5,7 +5,18 @@ export const ACCESS_TOKEN_URL = import.meta.env.VITE_ACCESS_TOKEN_PATH;
 export const FRONT_URL = import.meta.env.VITE_FRONT_URL;
 
 export const END_POINT = {
-    LOGIN: '/auth/login',
-    SIGN_UP: '/auth/signup',
-    CHECK_NICKNAME: '/check-nickname',
+    LOGIN: 'qufit/auth/login',
+    SIGN_UP: 'qufit/auth/signup',
+    CHECK_NICKNAME: 'qufit/check-nickname',
+};
+
+export const HttpStatus = {
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    NOT_FOUND: 404,
+    CONTENT_TOO_LARGE: 413,
+    INTERNAL_SERVER_ERROR: 500,
 };
--- front-end/src/apis/auth/AuthApi.tsx
+++ front-end/src/apis/auth/AuthApi.tsx
@@ -1,5 +1,6 @@
-import { ACCESS_TOKEN_URL, FRONT_URL, REDIRECT_URI, REST_API_KEY } from '@apis/ApiConstants';
-import axios from 'axios';
+import { ACCESS_TOKEN_URL, END_POINT, FRONT_URL, HttpStatus, REDIRECT_URI, REST_API_KEY } from '@apis/ApiConstants';
+import { instance } from '@apis/axios';
+import axios, { isAxiosError } from 'axios';
 
 export const getKakaoAccessToken = async (authCode: string) => {
     const header = { headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' } };
@@ -12,9 +13,22 @@ export const getKakaoAccessToken = async (authCode: string) => {
     };
 
     const response = await axios.post(ACCESS_TOKEN_URL, params, header);
-    console.log(response);
     //TODO: 예외처리해야함
     const accessToken = response.data.access_token;
 
-    return accessToken;
+    return response;
+};
+
+export const login = async (accessToken: string) => {
+    try {
+        const response = await instance.get(END_POINT.LOGIN, { params: { accessToken: accessToken } });
+        if (response.status === HttpStatus.OK) return response.data;
+    } catch (error) {
+        if (isAxiosError(error)) {
+            console.log(error);
+            // if (error.response.status === HttpStatus.UNAUTHORIZED) {
+            //     console.log(1111);
+            // }
+        }
+    }
 };
