282023255a4511f8a95e319af9924fdaeabbd2d6
박세은 <seeunbag30@gmail.com>
2024-07-30 13:56:23 +0900

    Design: 반응형 수치 지워진 것 되돌리기

--- front-end/src/components/common/Layout.tsx
+++ front-end/src/components/common/Layout.tsx
@@ -8,11 +8,13 @@ const Layout = () => {
     const background = isLocation === '/main' ? 'bg-mainPageBg' : 'bg-chatPageBg';
 
     return (
-        <div className={`flex flex-col items-center justify-center w-screen h-screen bg-cover px-14 ${background}`}>
+        <div
+            className={`flex flex-col items-center justify-center w-screen h-screen bg-cover px-14 py-4 ${background} lg:px-20 md:py-10 sm:py-12 sm:px-6 xs:py-12 xs:px-6`}
+        >
             <Header />
-            <div className="w-full aspect-layout effect-layout border-b-4 border-x-2 border-lightPurple-4 rounded-b-[2.75rem] relative">
+            <div className="w-full aspect-layout effect-layout border-b-4 border-x-2 border-lightPurple-4 rounded-b-[2.75rem] relative md:h-full lg:rounded-b-[2rem] sm:h-full xs:h-full">
                 <Outlet />
-                <div className="z-0 w-full h-full rounded-b-[2.75rem] bg-whitePink opacity-20" />
+                <div className="z-0 w-full h-full rounded-b-[2.75rem] bg-whitePink opacity-20 md:h-full lg:rounded-b-[2rem] sm:h-full xs:h-full" />
             </div>
         </div>
     );
