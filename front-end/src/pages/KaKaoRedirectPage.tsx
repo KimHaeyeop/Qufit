b95cfb8b09f37a4f825f76dfbb4fed03d6bbf99a
mung96 <mung5079@gmail.com>
2024-07-30 11:24:14 +0900

    #S11P12A209-121 #comment FE/ Fix : 소셜로그인 이후 KOE302 뜨면 IntroPage 리다이렉트

--- front-end/src/pages/KaKaoRedirectPage.tsx
+++ front-end/src/pages/KaKaoRedirectPage.tsx
@@ -3,6 +3,7 @@ import { getKakaoAccessToken, login } from '@apis/auth/AuthApi';
 import { PATH } from '@routers/PathConstants';
 import { useSetAccessTokenStore } from '@stores/auth/signUpStore';
 import { isAxiosError } from 'axios';
+import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const KakaoRedirectPage = () => {
@@ -10,23 +11,36 @@ const KakaoRedirectPage = () => {
     const navigate = useNavigate();
     const authCode = searchParams.get('code');
     const setAccessToken = useSetAccessTokenStore();
-    getKakaoAccessToken(authCode!).then((response) => {
-        setAccessToken(response.data.access_token);
-        login(response.data.access_token)
-            .then((response) => console.log(response))
-            .catch((error) => {
-                if (isAxiosError(error)) {
-                    console.log(error);
-                    if (error.response?.data.status === HttpStatus.UNAUTHORIZED) {
-                        if (error.response.data.errorMessage === '회원 가입이 필요합니다.') {
-                            navigate(PATH.SIGN_UP);
-                        } else if (error.response.data.errorMessage === '가입 승인 대기 중인 계정입니다.') {
-                            navigate(PATH.INTRODUCTION);
+
+    useEffect(() => {
+        getKakaoAccessToken(authCode!)
+            .then((response) => {
+                setAccessToken(response.data.access_token);
+                login(response.data.access_token)
+                    .then((response) => console.log(response))
+                    .catch((error) => {
+                        if (isAxiosError(error)) {
+                            console.log(error);
+                            if (error.response?.data.status === HttpStatus.UNAUTHORIZED) {
+                                if (error.response.data.errorMessage === '회원 가입이 필요합니다.') {
+                                    navigate(PATH.SIGN_UP);
+                                } else if (error.response.data.errorMessage === '가입 승인 대기 중인 계정입니다.') {
+                                    navigate(PATH.INTRODUCTION);
+                                }
+                            }
                         }
-                    }
+                    });
+            })
+            .catch((error) => {
+                if (error.response.data.error_code === 'KOE320') {
+                    // navigate();
+                    navigate(PATH.INTRODUCTION);
+                    setAccessToken('');
+                    localStorage.removeItem('accessToken');
+                    console.log(error);
                 }
             });
-    });
+    }, []);
 
     return <div>카카오 리다이렉트 페이지</div>;
 };
