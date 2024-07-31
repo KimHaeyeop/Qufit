1a4d69a6815138b9d48a4eb823dca08223db8ebe
mung96 <mung5079@gmail.com>
2024-07-31 17:43:50 +0900

    Feat: 회원가입 전용 컴포넌트 구현

--- front-end/src/components/common/input/Input.tsx
+++ front-end/src/components/common/input/Input.tsx
@@ -9,9 +9,15 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 const Input = ({ label, name, value, onChange, ...rest }: InputProps) => {
     return (
-        <label>
-            {label && <span>{label}</span>}
-            <input name={name} value={value} onChange={onChange} {...rest} />
+        <label className="flex flex-col gap-3">
+            {label && <p className="text-3xl">{label}</p>}
+            <input
+                className="relative w-full pr-20 bg-transparent border-4 rounded-full outline-none h-14 border-lightPurple-3 effect-purePink pl-7 placeholder:text-white placeholder:opacity-80 lg:h-12 md:h-20 md:text-xl md:pr-24 sm:h-20 sm:text-xl sm:pr-24 xs:h-20 xs:text-xl xs:pr-24"
+                name={name}
+                value={value}
+                onChange={onChange}
+                {...rest}
+            />
         </label>
     );
 };
