c228fae7cb88c7ba0e86cfb023a75910b7d876d6
mung96 <mung5079@gmail.com>
2024-08-01 14:23:50 +0900

    Feat: instance에 header와 interceptor설정

--- front-end/src/components/auth/LoginBtn.tsx
+++ front-end/src/components/auth/LoginBtn.tsx
@@ -1,11 +1,8 @@
-import { AUTH_CODE_PATH, FRONT_URL, REDIRECT_URI, REST_API_KEY } from '@apis/ApiConstants';
+import { KAKAO_LOGIN_URL } from '@apis/ApiConstants';
 import { Link } from 'react-router-dom';
 
 const LoginBtn = () => {
-    const KAKAO_URL = `${AUTH_CODE_PATH}?client_id=${REST_API_KEY}&redirect_uri=${
-        FRONT_URL + REDIRECT_URI
-    }&response_type=code`;
-    return <Link to={KAKAO_URL}>나를 누르면 카카오 소셜로그인을 할 수 있어</Link>;
+    return <Link to={KAKAO_LOGIN_URL}>나를 누르면 카카오 소셜로그인을 할 수 있어</Link>;
 };
 
 export default LoginBtn;
