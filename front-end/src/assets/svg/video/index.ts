c2941e5bdcc3eb0191cff6de101ce5e7d64a270d
mung96 <mung5079@gmail.com>
2024-08-02 15:22:51 +0900

    Feat: 타이머 기능 구현

--- front-end/src/assets/svg/video/index.ts
+++ front-end/src/assets/svg/video/index.ts
@@ -2,3 +2,4 @@ export { default as CameraOffIcon } from './CameraOffIcon';
 export { default as CrownIcon } from './CrownIcon';
 export { default as MicOffIcon } from './MicOffIcon';
 export { default as MicOnIcon } from './MicOnIcon';
+export { default as TimerIcon } from './TimerIcon';
