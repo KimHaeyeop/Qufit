41ec38d4c4dbcd3f84d78075833180b849fd8c66
박세은 <seeunbag30@gmail.com>
2024-08-05 09:18:04 +0900

    Fix: 충돌 수정

--- front-end/src/apis/axios.tsx
+++ front-end/src/apis/axios.tsx
@@ -2,10 +2,7 @@ import axios from 'axios';
 
 export const instance = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
-<<<<<<< HEAD
     headers: {
         Authorization: `Bearer ${import.meta.env.VITE_TEST_TOKEN}`,
     },
-=======
->>>>>>> 16750b6d79780c204661ccefb43be5cae9db438f
 });
--- front-end/src/components/chat/ChatInfo.tsx
+++ front-end/src/components/chat/ChatInfo.tsx
@@ -44,7 +44,6 @@ const ChatInfo = ({
         <div className="flex flex-col">
             <div className="w-full h-0.5 bg-hotPink" />
             <div className="flex items-center justify-between pl-1">
-<<<<<<< HEAD
                 <button onClick={handleOnClickButton} className="flex items-center w-full py-5 lg:py-3">
                     <img
                         src={profileImage}
@@ -60,22 +59,9 @@ const ChatInfo = ({
                         </div>
                         <p className="mt-2 text-xl font-light text-left text-white truncate max-w-96 lg:text-lg lg:mt-1 lg:max-w-72 xs:text-sm xs:mt-1 xs:max-w-36">
                             {lastMessage}
-=======
-                <button className="flex items-center w-full py-5 lg:py-3" />
-                <img
-                    src={profileImage}
-                    alt="user profile image"
-                    className="rounded-full w-14 h-14 lg:w-12 lg:h-12 xs:w-12 xs:h-12"
-                />
-                <div className="flex flex-col mx-3.5 lg:mx-2.5 xs:mx-1.5">
-                    <div className="flex items-center">
-                        <p className="mr-4 text-2xl text-white truncate max-w-72 lg:text-lg lg:mr-3 lg:max-w-60 xs:text-lg xs:max-w-32 xs:mr-1">
-                            {nickname}sssssssssssssssss
->>>>>>> 16750b6d79780c204661ccefb43be5cae9db438f
                         </p>
                         <p className="font-light text-white opacity-80 lg:text-sm xs:text-sm">14 : 32</p>
                     </div>
-<<<<<<< HEAD
                 </button>
                 <div className="flex">
                     {unreadCount > 0 && (
@@ -87,22 +73,6 @@ const ChatInfo = ({
                         <DoorExitIcon className="w-8 lg:w-7 md:w-10 xs:w-7" />
                     </button>
                 </div>
-=======
-                    <p className="mt-2 text-xl font-light text-white truncate max-w-96 lg:text-lg lg:mt-1 lg:max-w-72 xs:text-sm xs:mt-1 xs:max-w-36">
-                        {lastMessage}dddddddddddddddddddddd
-                    </p>
-                </div>
-            </div>
-            <div className="flex">
-                {unreadCount > 0 && (
-                    <div className="flex items-center justify-center w-8 h-8 mr-5 font-bold text-center text-white bg-white rounded-full bg-opacity-40 lg:w-7 lg:h-7 lg:text-sm lg:mr-3 md:w-10 md:h-10 md:mr-8 xs:w-7 xs:h-7 xs:text-sm xs:mr-4">
-                        {unreadCount}
-                    </div>
-                )}
-                <button>
-                    <DoorExitIcon className="w-8 lg:w-7 md:w-10 xs:w-7" />
-                </button>
->>>>>>> 16750b6d79780c204661ccefb43be5cae9db438f
             </div>
         </div>
     );
