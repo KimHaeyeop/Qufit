7222fbe5dc70989e8f09f190ec9675d2d2624ede
박세은 <seeunbag30@gmail.com>
2024-08-06 13:24:39 +0900

    Design: main 페이지 재디자인

--- front-end/src/components/main/Tag.tsx
+++ front-end/src/components/main/Tag.tsx
@@ -1,12 +1,12 @@
 interface TagProps {
     tag: string;
+    isList: boolean;
 }
 
-const Tag = ({ tag }: TagProps) => {
+const Tag = ({ tag, isList }: TagProps) => {
     return (
-        <div className="flex items-center justify-center mb-2 mr-3 border-2 rounded-full max-w-36 border-lightPurple-6 effect-hotPink px-7 h-7 lg:px-4 lg:h-6 lg:mr-1.5">
-            <span className="z-10 font-medium text-left text-white truncate lg:text-sm"># {tag}</span>
-            <div className="absolute z-0 w-full h-full rounded-full bg-hotPink opacity-40" />
+        <div className="flex items-center justify-center mr-2.5 bg-white bg-opacity-30 rounded-full px-2.5 h-7 lg:px-4 lg:h-6 lg:mr-1.5">
+            <span className="text-sm font-medium text-left text-white truncate">{isList ? `# ${tag}` : `${tag}`}</span>
         </div>
     );
 };
