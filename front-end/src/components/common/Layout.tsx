f304487817ec9040880550b4c44117d3f24f461d
박세은 <seeunbag30@gmail.com>
2024-08-06 13:24:11 +0900

    Design: 공통 컴포넌트 재디자인

--- front-end/src/components/common/Layout.tsx
+++ front-end/src/components/common/Layout.tsx
@@ -1,22 +1,61 @@
 import LoginBtn from '@components/auth/LoginBtn';
+import LottieComponent from '@components/common/LottieComponent';
 import Header from '@components/common/Header';
+import FireFly from '@assets/lottie/firefly.json';
+import StarFalling from '@assets/lottie/starFalling.json';
 import { Outlet, useLocation } from 'react-router-dom';
 
 const Layout = () => {
     const location = useLocation();
     const isLocation = location.pathname;
 
-    const background = isLocation === '/main' ? 'bg-mainPageBg' : 'bg-chatPageBg';
+    const background = isLocation === '/main' ? 'bg-mainPageBg' : 'bg-black';
 
     return (
         <div
-            className={`flex flex-col items-center justify-center w-screen h-screen bg-cover px-14 py-4 ${background} lg:px-20 md:py-10 sm:py-12 sm:px-6 xs:py-12 xs:px-6`}
+            className={`relative flex flex-col items-center justify-center max-w-screen h-screen bg-cover px-16 py-4 ${background} lg:px-20 md:py-10 sm:py-12 sm:px-6 xs:py-12 xs:px-6`}
         >
             <Header />
-            <div className="w-full aspect-layout effect-layout border-b-4 border-x-2 border-lightPurple-4 rounded-b-[2.75rem] relative md:h-full lg:rounded-b-[2rem] sm:h-full xs:h-full">
+            <div className="w-full z-10 aspect-layout effect-layout rounded-b-[2.5rem] relative md:h-full lg:rounded-b-[2rem] sm:h-full xs:h-full">
                 <Outlet />
-                <div className="z-0 w-full h-full rounded-b-[2.75rem] bg-whitePink opacity-20 md:h-full lg:rounded-b-[2rem] sm:h-full xs:h-full" />
             </div>
+            {isLocation === '/main' ? (
+                <>
+                    <LottieComponent
+                        animationData={FireFly}
+                        speed={0.3}
+                        isPaused={false}
+                        isStopped={false}
+                        loop={true}
+                        init={0}
+                        end={100}
+                        className="absolute left-[-0.625rem] z-0 w-64 h-40 transform bottom-5 -scale-x-100"
+                    />
+                    <LottieComponent
+                        animationData={FireFly}
+                        speed={0.3}
+                        isPaused={false}
+                        isStopped={false}
+                        loop={true}
+                        init={0}
+                        end={100}
+                        className="absolute right-0 z-0 w-56 bottom-10"
+                    />
+                </>
+            ) : (
+                <>
+                    <LottieComponent
+                        animationData={StarFalling}
+                        speed={0.3}
+                        isPaused={false}
+                        isStopped={false}
+                        loop={true}
+                        init={0}
+                        end={100}
+                        className="absolute z-0 w-full h-full"
+                    />
+                </>
+            )}
         </div>
     );
 };
