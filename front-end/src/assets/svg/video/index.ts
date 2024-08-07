b42e03fab243c435d068306c94a23459c5e475df
mung96 <mung5079@gmail.com>
2024-08-07 17:15:14 +0900

    Design: 게임전 화면 디자인

--- front-end/src/assets/svg/video/index.ts
+++ front-end/src/assets/svg/video/index.ts
@@ -4,4 +4,5 @@ export { default as FemaleIcon } from './FemaleIcon';
 export { default as MaleIcon } from './MaleIcon';
 export { default as MicOffIcon } from './MicOffIcon';
 export { default as MicOnIcon } from './MicOnIcon';
+export { default as PlayIcon } from './PlayIcon';
 export { default as TimerIcon } from './TimerIcon';
