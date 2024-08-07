8d390c8ea02e27dc043d1f962e5d6ecf376e6954
mung96 <mung5079@gmail.com>
2024-08-07 14:31:14 +0900

    Design: GameStartButton 디자인

--- front-end/src/components/video/GameStartButton.tsx
+++ front-end/src/components/video/GameStartButton.tsx
@@ -5,17 +5,35 @@ import useRoom from '@hooks/useRoom';
 
 const GameStartButton = () => {
     const [isStart, setIsStart] = useState(false);
-    const { isManager } = useRoom();
+    const { isHost } = useRoom();
 
     const gameStart = () => {
-        if (isManager) {
+        if (isHost) {
             setIsStart(true);
         }
     };
     return (
         <div className="relative flex flex-col items-center">
-            <div className="flex gap-8 bg-black group" onClick={gameStart}>
+            <div className="flex gap-8 group" onClick={gameStart}>
                 {!isStart ? (
+                    <>
+                        <LottieComponent
+                            animationData={animateData}
+                            speed={0.3}
+                            isPaused={false}
+                            isStopped={false}
+                            loop={true}
+                            init={0}
+                            end={0.1}
+                            className="w-[20rem]"
+                        />
+                        {isHost && (
+                            <p className="absolute text-4xl text-white -translate-x-1/2 -translate-y-1/2 animate-pulse top-1/2 left-1/2">
+                                Start
+                            </p>
+                        )}
+                    </>
+                ) : (
                     <LottieComponent
                         animationData={animateData}
                         speed={0.5}
@@ -23,17 +41,6 @@ const GameStartButton = () => {
                         isStopped={false}
                         loop={false}
                         init={0}
-                        end={86}
-                        className="w-[20rem] animate-pulse"
-                    />
-                ) : (
-                    <LottieComponent
-                        animationData={animateData}
-                        speed={0.3}
-                        isPaused={false}
-                        isStopped={false}
-                        loop={true}
-                        init={86}
                         className="w-[20rem]"
                     />
                 )}
@@ -41,7 +48,7 @@ const GameStartButton = () => {
 
             {!isStart && (
                 <p className="absolute text-lg text-white -bottom-5">
-                    {isManager ? '미팅을 시작하려면 버튼을 눌러주세요' : '잠시후 미팅이 시작됩니다. 기다려주세요!'}
+                    {isHost ? '미팅을 시작하려면 버튼을 눌러주세요' : '잠시후 미팅이 시작됩니다. 기다려주세요!'}
                 </p>
             )}
         </div>
