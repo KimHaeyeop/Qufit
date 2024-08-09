5ed082d3278e4ba731461e1a0a608e8c7e773e30
mung96 <mung5079@gmail.com>
2024-08-09 16:59:24 +0900

    Feat: 게임 결과 확인 구현

--- front-end/src/components/game/step/GameResult.tsx
+++ front-end/src/components/game/step/GameResult.tsx
@@ -8,17 +8,39 @@ import { PATH } from '@routers/PathConstants';
 import { useState } from 'react';
 import ChoiceGroup from '@components/game/ChoiceGroup';
 import Choice from '@components/game/Choice';
+import useRoom from '@hooks/useRoom';
 
 interface GameResultProps {
-    onStop?: () => void;
-    onNext?: () => void;
-    title?: string;
-    senario1?: string;
-    senario2?: string;
+    onStop: () => void;
+    onNext: () => void;
+    id: number;
+    title: string;
+    scenario1: string;
+    scenario2: string;
+    scenario1Cnt: number;
+    scenario2Cnt: number;
+    nullCnt: number;
 }
 
-const GameResult = ({ id, title, senario1, senario2, onNext, onStop }: GameResultProps) => {
+const GameResult = ({
+    id,
+    title,
+    scenario1,
+    scenario2,
+    onNext,
+    onStop,
+    scenario1Cnt,
+    scenario2Cnt,
+    nullCnt,
+}: GameResultProps) => {
     const [answer, setAnswer] = useState('12');
+    const { isHost } = useRoom();
+
+    const handleStopBtnClick = () => {
+        if (isHost) {
+            onStop();
+        }
+    };
 
     return (
         <div className="relative flex items-center justify-center p-3 bg-black aspect-gameBg">
@@ -27,11 +49,20 @@ const GameResult = ({ id, title, senario1, senario2, onNext, onStop }: GameResul
             </div>
 
             <div className="absolute bottom-[1.5rem] gap-[1rem] flex flex-col w-[calc(100%-5rem)] p-[2rem] bg-black opacity-50 min-h-[10rem]">
-                <TypingText frame={50} text={'[결과]  '} className="w-full text-xl font-bold text-white" />
-
-                <TypingText frame={80} text={title + '  '} className="w-full text-lg text-white" />
+                <TypingText frame={50} text={title + '  '} className="w-full text-xl font-bold text-white" />
+                <TypingText
+                    frame={50}
+                    text={scenario1 + ':  ' + scenario1Cnt + '명  '}
+                    className="w-full text-lg text-white"
+                />
+                <TypingText
+                    frame={50}
+                    text={scenario2 + ':  ' + scenario2Cnt + '명  '}
+                    className="w-full text-lg text-white"
+                />
+                <TypingText frame={50} text={'선택안함:  ' + nullCnt + '명  '} className="w-full text-lg text-white" />
                 <div
-                    onClick={onStop}
+                    onClick={handleStopBtnClick}
                     className="border-2 border-white rounded-lg absolute z-100 -top-4 right-0 -translate-y-full flex flex-col items-start py-[1.5rem] gap-1 px-[1rem] bg-black opacity-50"
                 >
                     <div className="w-full  opacity-90 p-2 items-center text-xl  text-white justify-center   hover:bg-lightPurple-2  has-[:checked]:animate-pulse">
