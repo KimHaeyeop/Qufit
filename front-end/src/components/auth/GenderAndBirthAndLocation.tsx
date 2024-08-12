093db2c59e327593c4d4006017bbf5b1a47b9e2a
mung96 <mung5079@gmail.com>
2024-08-12 16:52:07 +0900

    Feat: useForm변경

--- front-end/src/components/auth/GenderAndBirthAndLocation.tsx
+++ front-end/src/components/auth/GenderAndBirthAndLocation.tsx
@@ -6,10 +6,10 @@ import Radio from '@components/common/radio/Radio';
 import RadioGroup from '@components/common/radio/RadioGroup';
 
 import { GENDER, LOCATION } from '@components/mypage/SignupConstants';
-import SingleTag from '@components/mypage/SingleTag';
-import SingleTagGroup from '@components/mypage/SingleTagGroup';
+
 import useForm from '@hooks/useForm';
 import signupValidate from '@utils/signupValidate';
+import { useEffect } from 'react';
 import Select from 'react-select';
 
 interface GenderAndBirthAndLocationProps {
@@ -20,11 +20,15 @@ interface GenderAndBirthAndLocationProps {
 const GenderAndBirthAndLocation = ({ onNext, registData }: GenderAndBirthAndLocationProps) => {
     const locations = LOCATION.map((location) => ({ label: location.name, value: location.code }));
     const years = Array.from({ length: 100 }, (_, idx) => ({ label: `${2022 - idx}년`, value: 2022 - idx }));
-    const { values, messages, valids, handleChange, handleSubmit } = useForm({
+    const { values, messages, valids, handleChange, handleSubmit, handleSelectChange } = useForm({
         initialValues: registData,
         onSubmit: onNext,
         validate: signupValidate,
     });
+
+    useEffect(() => {
+        console.log(values);
+    }, [values]);
 
     return (
         <div className="flex flex-col items-center gap-3">
@@ -36,8 +40,18 @@ const GenderAndBirthAndLocation = ({ onNext, registData }: GenderAndBirthAndLoca
                 ))}
             </RadioGroup>
 
-            <Select options={years} className="w-full" placeholder="태어난 연도" />
-            <Select options={locations} placeholder="지역" className="w-full" />
+            <Select
+                options={years}
+                onChange={(e) => handleSelectChange(e, 'birthYear')}
+                className="w-full"
+                placeholder="태어난 연도"
+            />
+            <Select
+                options={locations}
+                onChange={(e) => handleSelectChange(e, 'locationId')}
+                placeholder="지역"
+                className="w-full"
+            />
         </div>
     );
 };
--- front-end/src/components/auth/Nickname.tsx
+++ front-end/src/components/auth/Nickname.tsx
@@ -1,8 +1,6 @@
-interface NicknameStepProps {
-    count: number;
-}
+interface NicknameProps {}
 
-const NicknameStep = ({ count }: NicknameStepProps) => {
+const Nickname = ({}: NicknameProps) => {
     return (
         <div className="flex items-center gap-3">
             <p>큐핏에서 사용할 닉네임을 입력해주세요</p>
@@ -10,4 +8,4 @@ const NicknameStep = ({ count }: NicknameStepProps) => {
     );
 };
 
-export default NicknameStep;
+export default Nickname;
