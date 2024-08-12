7d9556d870ed41bb671d980c9ed4fcad2b8d8ce5
jhw <612gpdnjs@gmail.com>
2024-08-12 13:26:46 +0900

    #S11P12A209-298 #comment FE design:정보 입력 컴포넌트 ui 수정

--- front-end/src/components/common/input/TextArea.tsx
+++ front-end/src/components/common/input/TextArea.tsx
@@ -9,10 +9,10 @@ interface TextAreaProps extends InputHTMLAttributes<HTMLTextAreaElement> {
 }
 const TextArea = ({ label, onChange, name, value, rows, ...rest }: TextAreaProps) => {
     return (
-        <label className="flex flex-col gap-3">
-            {label && <p className="text-3xl">{label}</p>}
+        <label className="flex flex-col gap-1">
+            {label && <p className="font-semibold text-white text-l">{label}</p>}
             <textarea
-                className="relative w-full px-4 py-8 pr-20 bg-transparent border-4 outline-none resize-none rounded-3xl border-lightPurple-3 effect-purePink pl-7 placeholder:text-white placeholder:opacity-80 lg:h-12 md:h-20 md:text-xl md:pr-24 sm:h-20 sm:text-xl sm:pr-24 xs:h-20 xs:text-xl xs:pr-24"
+                className="relative w-full h-10 px-5 py-8 text-white bg-transparent border-2 border-white outline-none resize-none text-md border-t-transparent border-r-transparent border-l-transparent scrollbar-hide placeholder:text-white placeholder:opacity-80 lg:h-12 md:h-20 md:text-sm md:pr-5 sm:h-15 sm:text-xs sm:pr-5 xs:h-15 xs:text-xs xs:pr-5"
                 name={name}
                 value={value}
                 onChange={onChange}
