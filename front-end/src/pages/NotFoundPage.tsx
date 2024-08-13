6015ebf42c619a8a80acb5238517d091bd4b0155
송현명 <mung5079@gmail.com>
2024-08-13 19:19:36 +0900

    Revert "Merge branch 'fix/FE-builderror수정' into 'dev'"
    This reverts merge request !124

--- front-end/src/pages/NotFoundPage.tsx
+++
@@ -1,5 +0,0 @@
-const NotFoundPage = () => {
-    return <div>페이지를 찾을수 없습니다.</div>;
-};
-
-export default NotFoundPage;
--- front-end/src/utils/generateSelectOptions.tsx
+++
@@ -1,9 +0,0 @@
-const generateSelectOptions = (start: number, end: number) => {
-    const options = Array.from({ length: end - start + 1 }, (_, i) => start + i);
-    return options.map((option) => ({
-        value: option.toString(),
-        label: option.toString(),
-    }));
-};
-
-export default generateSelectOptions;
