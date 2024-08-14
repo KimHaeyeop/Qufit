7a1015dfe231f00dc935121c22620bfbe8cb0627
jhw <612gpdnjs@gmail.com>
2024-08-14 19:23:44 +0900

    #S11P12A209-308 #comment BE feat : 관리자 로그인 페이지 개발

---
+++ front-end/src/pages/Admin.tsx
@@ -0,0 +1,12 @@
+import AdminLogin from "@components/admin/AdminLogin"
+
+const Admin = () => {
+
+    return(
+        <main className="flex flex-col items-center justify-center h-full bg-lightPurple-7">
+            <AdminLogin/>
+        </main>
+    )
+}
+
+export default Admin;
