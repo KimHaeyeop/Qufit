7409cfe510ebeace3e02b2337dd0ca58796a7937
mung96 <mung5079@gmail.com>
2024-08-14 15:15:38 +0900

    Feat: 그룹비디오 페이지 구현

--- front-end/src/components/game/step/Loading.tsx
+++ front-end/src/components/game/step/Loading.tsx
@@ -5,8 +5,10 @@ interface LoadingProps {
     onNext: () => void;
 }
 const Loading = ({ onNext }: LoadingProps) => {
-    const restSec = useTimer(LOADING_SEC, onNext);
-    console.log(restSec);
+    // const restSec = useTimer(LOADING_SEC, onNext);
+    // console.log(restSec);
+
+    useTimer(LOADING_SEC, onNext);
     return (
         <div className="relative flex items-center justify-center p-3 bg-black aspect-gameBg">
             <div className="flex justify-center rounded-lg opacity-0 item-center">
--- front-end/src/modals/video/MoveRoomModal.tsx
+++ front-end/src/modals/video/MoveRoomModal.tsx
@@ -1,25 +1,52 @@
+import { GROUP_VIDEO_END_SEC } from '@components/game/Constants';
+import useTimer from '@hooks/useTimer';
+import { useEffect, useState } from 'react';
+
 interface MoveRoomModalProps {
-    onClose: () => void;
     onClick: () => void;
+    onClose: () => void;
+    message: string;
+    /*
+    다대다 -> 일대일 : '잠시 후 일대일 미팅으로 이동됩니다.'
+    일대일 -> 일대일
+    : '친구 추가 성공 ? 친구 추가에 성공하였습니다 : 친구 추가에 실패하였습니다.
+       잠시 후 일대일 미팅으로 이동합니다.'
+    일대일 -> 종료
+    : '잠시 후 미팅이 종료됩니다. 수고하셨습니다? 감사합니다?'
+    */
 }
 
-const MoveRoomModal = ({ onClose, onClick }: MoveRoomModalProps) => {
+const MoveRoomModal = ({ onClose, message, onClick }: MoveRoomModalProps) => {
+    const [count, setCount] = useState(5);
+
+    useEffect(() => {
+        if (count === 0) {
+            onClose(); // 카운트다운이 0이 되면 모달 닫기 -> 다음 페이지로 이동
+            onClick();
+            return;
+        }
+        const timer = setInterval(() => {
+            setCount((prev) => prev - 1);
+        }, 1000);
+
+        return () => clearInterval(timer); // 컴포넌트 언마운트 시 타이머 정리
+    }, [count, onClose]);
     return (
         <>
             <div className="fixed top-0 left-0 w-screen h-screen bg-Dark_Layout-100 bg-opacity-60" onClick={onClose} />
-            <div className="absolute flex flex-col items-center gap-5 -translate-x-1/2 -translate-y-1/2 border-none p-9 bg-Light_Layout-400 text-Light_Text_Name top-1/2 left-1/2 rounded-2xl">
-                <p className="text-white">미팅 시간이 종료되었어요. 다음 방으로 이동해주세요.</p>
-                <div className="flex w-full gap-3">
-                    <button
-                        onClick={() => {
-                            onClick();
-                            onClose();
-                        }}
-                        className="w-full text-smokeWhite h-9 bg-Light_Layout-100 text-Light_Text_AboutMe text-base rounded-[2rem] "
-                    >
-                        이동하기
-                    </button>
-                </div>
+            <div className="flex flex-col items-center p-10 text-white bg-transparent effect-purePink rounded-2xl gap-9">
+                <h2 className="flex p-3 text-lg font-bold text-center">
+                    {'미팅 시간이 종료되었어요. 다음 방으로 이동해주세요.'}
+                </h2>
+                <button
+                    onClick={() => {
+                        onClick();
+                        onClose();
+                    }}
+                    className="w-full text-smokeWhite h-9 bg-Light_Layout-100 text-Light_Text_AboutMe text-3xl font-bold rounded-[2rem] "
+                >
+                    이동하기
+                </button>
             </div>
         </>
     );
