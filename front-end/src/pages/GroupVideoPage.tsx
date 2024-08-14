89d53c403303b43c6f6fb3053b1be0442acfccb5
mung96 <mung5079@gmail.com>
2024-08-15 01:45:24 +0900

    fix builderror수정

--- front-end/src/pages/GroupVideoPage.tsx
+++ front-end/src/pages/GroupVideoPage.tsx
@@ -1,4 +1,4 @@
-import { useOtherIdxStore, useRoomParticipantsStore, useSetRoomIdStore } from '@stores/video/roomStore';
+import { useOtherIdxStore, useSetRoomIdStore } from '@stores/video/roomStore';
 import useRoom from '@hooks/useRoom';
 import ParticipantVideo from '@components/video/ParticipantVideo';
 import { useEffect, useRef, useState } from 'react';
