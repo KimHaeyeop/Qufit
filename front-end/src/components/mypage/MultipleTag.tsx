dfc3212ef442e446e40ecef6c0ad7fa0c4cb3152
jhw <612gpdnjs@gmail.com>
2024-08-14 12:55:43 +0900

    #S11P12A209-305 #comment FE feat : 마이페이지 정보 입력 컴포넌트 isUpdate 변수 설정

--- front-end/src/components/mypage/MultipleTag.tsx
+++ front-end/src/components/mypage/MultipleTag.tsx
@@ -4,13 +4,15 @@ import { ReactNode } from 'react';
 interface MultipleTagProps {
     children: ReactNode;
     value: string;
+    isUpdate: boolean;
 }
 
-const MultipleTag = ({ children, value, ...rest }: MultipleTagProps) => {
+const MultipleTag = ({ children, value, isUpdate, ...rest }: MultipleTagProps) => {
     return (
         <Checkbox
             value={value}
-            className="flex group bg-opacity-50 items-center h-5 bg-white justify-center mb-2 mr-1.5 border-none rounded-2xl has-[:checked]:bg-opacity-70 has-[:checked]:bg-pink hover:bg-pink hover:bg-opacity-60 px-3.5 py-3.5 lg:h-1 lg:mr-3"
+            className={`flex items-center h-5 has-[:checked]:bg-pink/70 bg-white/50 justify-center mb-2 mr-1.5 border-none rounded-2xl px-3.5 py-3.5 lg:mr-3 group  bg-opacity-60 ${isUpdate ? 'hover:bg-pink/60' : ''}`}
+            isUpdate={isUpdate} 
             {...rest}
         >
             <span className="z-10 text-xs text-left text-white truncate">
