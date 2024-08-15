da2f4c7c148099792e5a12dd2a76113a7c534080
박세은 <seeunbag30@gmail.com>
2024-08-15 20:55:31 +0900

    Feat: 태그 리스트에서 삭제 기능 추가

--- front-end/src/stores/video/tagFilterStore.tsx
+++ front-end/src/stores/video/tagFilterStore.tsx
@@ -1,20 +1,15 @@
 import { create } from 'zustand';
 
 interface TagFilterProps {
-    hobbyTagsId: number[];
-    setHobbyTagsId: (list: number[]) => void;
-    idealTypeTagsId: number[];
-    setIdealTypeTagsId: (list: number[]) => void;
+    tagsId: number[];
+    setTagsId: (list: number[]) => void;
     tags: string[];
     setTags: (list: string[]) => void;
 }
 
 const useTagFilterStore = create<TagFilterProps>((set) => ({
-    hobbyTagsId: [],
-    setHobbyTagsId: (list: number[]) => set(() => ({ hobbyTagsId: list })),
-
-    idealTypeTagsId: [],
-    setIdealTypeTagsId: (list: number[]) => set(() => ({ idealTypeTagsId: list })),
+    tagsId: [],
+    setTagsId: (list: number[]) => set(() => ({ tagsId: list })),
 
     tags: [],
     setTags: (list: string[]) => set(() => ({ tags: list })),
