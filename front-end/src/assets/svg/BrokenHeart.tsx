97297c856dcb66043b46dd4e15132c6c6e49f8de
mung96 <mung5079@gmail.com>
2024-08-16 00:14:54 +0900

    feat:밸런스게임 결과UI

---
+++ front-end/src/assets/svg/BrokenHeart.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgBrokenHeart = (props: SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 32 33" {...props}>
+        <path fill="#fff" d="M0 32.24h32v-32H0z" />
+    </svg>
+);
+export default SvgBrokenHeart;
