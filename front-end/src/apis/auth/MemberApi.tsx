c37e389d182fcb38056c986f54c22fd187203fee
박세은 <seeunbag30@gmail.com>
2024-08-16 04:08:27 +0900

    Feat: 마이페이지 프로필사진 AI 생성 API 연결

--- front-end/src/apis/auth/MemberApi.tsx
+++ front-end/src/apis/auth/MemberApi.tsx
@@ -10,11 +10,15 @@ export const putMemberInfo = async (data: MemberInfoDTO) => {
 export const getMemberList = async (page: number, size: number, sort?: string, status?: string) => {
     // params 객체에 page, size, sort, status를 포함시킵니다.
     const params: { page: number; size: number; sort?: string; status?: string } = {
-      page,
-      size,
-      sort,
-      status,
+        page,
+        size,
+        sort,
+        status,
     };
-  
-    return await instance.get(END_POINT.ADMIN + "/users", { params });
-  };
+
+    return await instance.get(END_POINT.ADMIN + '/users', { params });
+};
+
+export const postProfile = async () => {
+    return await instance.post(END_POINT.PROFILE);
+};
