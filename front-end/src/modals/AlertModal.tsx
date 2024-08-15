f3fd379b558e3752335e716f6f70069ab2e56668
박세은 <seeunbag30@gmail.com>
2024-08-15 13:05:12 +0900

    Feat: 알림용 모달

--- front-end/src/modals/AlertModal.tsx
+++ front-end/src/modals/AlertModal.tsx
@@ -1,12 +1,12 @@
 interface alertModalProps {
-    content: string;
+    contents: string;
     onClose: () => void;
 }
 
-const alertModal = ({ content, onClose }: alertModalProps) => {
+const alertModal = ({ contents, onClose }: alertModalProps) => {
     return (
         <div className="flex flex-col items-center justify-center px-10 py-10 shadow-xl bg-smokeWhite rounded-3xl">
-            <p className="text-xl text-center text-black whitespace-pre-wrap">{content}</p>
+            <p className="text-xl text-center text-black whitespace-pre-wrap">{contents}</p>
             <button
                 onClick={onClose}
                 className="px-10 py-2 mt-8 font-bold text-white rounded-full bg-pink bg-opacity-80"
