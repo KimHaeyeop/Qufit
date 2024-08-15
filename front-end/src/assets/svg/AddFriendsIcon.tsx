462f18120c8e1c2fcb1fd074e4282bf9f66accc1
mung96 <mung5079@gmail.com>
2024-08-16 01:00:49 +0900

    feat: 루트에 소개페이지

--- front-end/src/assets/svg/AddFriendsIcon.tsx
+++ front-end/src/assets/svg/AddFriendsIcon.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgAddFriendsIcon = (props: SVGProps<SVGSVGElement>) => (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 900 900" {...props}>
